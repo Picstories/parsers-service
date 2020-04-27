b65c3ce20aff940814c61f6e4c8dabf15768d608
arman.shamenov <arman.shamenov@corp.mail.ru>
2020-04-27 11:21:49 +0500

    enable filter in spring annotation

--- src/main/java/org/picstories/parsers/ParsersApplication.java
+++ src/main/java/org/picstories/parsers/ParsersApplication.java
@@ -1,12 +1,15 @@
 package org.picstories.parsers;
 
+import org.picstories.library.elasticsearch.ReactiveElasticsearchConfiguration;
+import org.picstories.library.mongodb.ReactiveMongoDbConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author arman.shamenov
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {ReactiveMongoDbConfiguration.class,
+        ReactiveElasticsearchConfiguration.class})
 public class ParsersApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParsersApplication.class, args);
